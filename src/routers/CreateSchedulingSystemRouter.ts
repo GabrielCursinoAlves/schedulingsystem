284901027c1438d5a618f0b66e55757a2460c828
ADSUnifeob2023 <gabriel.cursino@sou.unifeob.edu.br>
2025-10-14 23:05:00 -0300

    chore(setup): configure Fastify + Zod and add initial route

---
+++ src/routers/CreateSchedulingSystemRouter.ts
@@ -0,0 +1,15 @@
+import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import {FastifyInstance} from "fastify";
+
+import {SchemaCreateSystem} from "../config/schema/CreateSchedulingSystemSchema.ts";
+import {ControllerSystem} from "../controller/index.ts";
+
+export const CreateSchedulingRouter = async(app:FastifyInstance) => {
+
+  app.setValidatorCompiler(validatorCompiler);
+  app.setSerializerCompiler(serializerCompiler);
+
+  app.withTypeProvider<ZodTypeProvider>().post("/CreateSchedulingSystem", SchemaCreateSystem, new ControllerSystem.CreateSchedulingSystem().handle);
+
+}
