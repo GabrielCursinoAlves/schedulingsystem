3a2f8b0c0df6ed54afa0146c0d823ad1ee0ff882
ADSUnifeob2023 <gabriel.cursino@sou.unifeob.edu.br>
2025-10-15 23:07:06 -0300

    chore(setup): schema configuration for validating route data

--- src/config/schema/CreateSchedulingSystemSchema.ts
+++ src/config/schema/CreateSchedulingSystemSchema.ts
@@ -1,18 +1,37 @@
 import z from "zod";
 
+const HoursPatterns = z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/);
+const DayOfWeekSchema = z.enum(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]);
+
+const OnceSchema = z.object({ 
+  type: z.literal("once") 
+});
+
+const DailySchema = z.object({ 
+  type: z.literal("daily"),
+  time_of_day: HoursPatterns
+});
+
+const WeeklySchema = z.object({ 
+  type: z.literal("week"),
+  time_of_day: HoursPatterns,
+  day_of_week: DayOfWeekSchema  
+});
+
 export const SchemaCreateSystem = {
    schema: {
     body: z.object({
-      payload: z.array(
-        z.object({
-          type: z.string().min(8),
-          user_id: z.uuid(),
-          message: z.string().min(20)
-        })
-      ),
+      payload: z.object({
+        type: z.enum(["send_sms", "send_alert"]),
+        user_id: z.uuid(),
+        message: z.string()
+      }),
       run_at: z.coerce.date(),
-      recurrence_pattern: z.string(),
-      is_recurring: z.boolean()
+      recurrence_pattern: z.discriminatedUnion("type", [
+        OnceSchema,
+        DailySchema,
+        WeeklySchema
+      ]),
     })
   },
 }
