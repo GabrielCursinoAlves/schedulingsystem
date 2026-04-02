230fb79c179c73e603f237f421ced54bb691743b
GabrielCursinoAlves <ga72147@gmail.com>
2026-04-01 22:37:43 -0300

    feat(app): integrate RabbitMQ bootstrap

--- schedulerservice/src/lifecycle/bootstrap.ts
+++ schedulerservice/src/lifecycle/bootstrap.ts
@@ -1,7 +1,7 @@
 import { RabbitMQConnection } from "@/infrastructure/messaging/RabbitMQConnection.js";
 import { RabbitMQTopology } from "@/infrastructure/messaging/RabbitMQTopology.js";
 import { ControllerZodInstance } from "@/types/zod/InstanceZodType.js";
-import { gracefulShutdown } from "./gracefulShutdown.js";
+import { gracefulShutdown } from "./gracefulShutdown.js"; 
 
 export async function bootstrap(app: ControllerZodInstance) {
   const connectionRabbitMQ = RabbitMQConnection.getInstance();
