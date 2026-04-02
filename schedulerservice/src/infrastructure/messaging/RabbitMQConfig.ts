7f16da3d64e1946f4b1e5847456c673dfa759cbb
GabrielCursinoAlves <ga72147@gmail.com>
2026-04-01 22:24:34 -0300

    feat(rabbitmq): centralize messaging configuration (exchange, queues, routing keys)

--- schedulerservice/src/infrastructure/messaging/RabbitMQConfig.ts
+++ schedulerservice/src/infrastructure/messaging/RabbitMQConfig.ts
@@ -7,7 +7,7 @@ export const configRabbitMQ = {
     job: "job.queue",
     delay: "job.delay",
     dlq: "job.dlq"
-  },
+  }, 
   routingKey: {
     create_job: "job.create",
     dead_job: "job.dead"
