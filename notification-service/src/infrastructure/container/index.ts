23ce7d4a3f9833ea9e19351451ab5b32da749ee5
GabrielCursinoAlves <ga72147@gmail.com>
2026-08-27 22:40:14 -0300

    chore(notification): registers Twilio/Gmail in the DI container

--- notification-service/src/infrastructure/container/index.ts
+++ notification-service/src/infrastructure/container/index.ts
@@ -1,9 +1,13 @@
 import { NotificationHandler } from "@/services/NotificationHandlerService.js";
 import { RepositoriesSystem } from "@/repositories/index.js";
+import { TwilioService } from "@/services/TwilioService.js";
+import { GmailService } from "@/services/GmailService.js";
 
 const ControllerNotificationDi = {
   notificationHandler: new NotificationHandler(
-    new RepositoriesSystem.CreateNotification()
+    new RepositoriesSystem.CreateNotification(),
+    new GmailService(),
+    new TwilioService(),
   ),
 };
 
